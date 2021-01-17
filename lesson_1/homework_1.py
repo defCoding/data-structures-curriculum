0252be67670b0d4af312f1c4469608f04d74622d
Kevin Cao <kcao1998@gmail.com>
2021-01-17 18:28:27 -0500

    first commit

---
+++ lesson_1/homework_1.py
@@ -0,0 +1,34 @@
+import timeit
+
+def time_function(func, *args):
+    """
+    Calculates the runtime of a function with the given arguments in milliseconds.
+
+    Params:
+    func (Function): The function to be timed.
+    *args ([Any]): The list of arguments to be passed to the function.
+
+    Return:
+    Number: The number of milliseconds it took for the function to run.
+    """
+    return round(timeit.timeit(lambda: func(*args), number=1) * 1000, 3)
+
+
+def linear(n):
+    # Create a function that runs in linear time.
+    # TODO
+
+def polynomial(n):
+    # Create a function that runs in polynomial (n^2) time.
+    # TODO
+    pass
+
+def logarithmic(n):
+    # Create a function that runs in logarithmic time.
+    # TODO
+    pass
+
+def nlogn(n):
+    # Create a function that runs in nlogn time.
+    # TODO
+    pass
---
+++ lesson_1/notes.md
@@ -0,0 +1,14 @@
+# Lesson 1 Lecture Notes
+
+## What is Time Complexity?
+
+- Basic Introduction
+  - Time Complexity Describes Growth, not Speed
+  - Big-O Notation
+  - Graphing Time Complexity
+- How to Calculate Time Complexity
+  - Time Complexity using Loops
+    - Briefly Explain Pseudo Time-Complexity
+  - Insertion Sort
+  - Merge Sort
+  - Finding Min/Max in Array
